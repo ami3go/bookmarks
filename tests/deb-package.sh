b1aadbba7996b85d468decc0af4045b084ac68db
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-11 14:06:12 +0300

    Expect Debian revision 3 in package tests

--- tests/deb-package.sh
+++ tests/deb-package.sh
@@ -2,7 +2,7 @@
 set -eu
 
 VERSION="$(sed -n 's/^[[:space:]]*"version":[[:space:]]*"\([^"]*\)".*/\1/p' package.json | head -n 1)"
-DEB_REVISION="${DEB_REVISION:-2}"
+DEB_REVISION="${DEB_REVISION:-3}"
 DEB_VERSION="${VERSION}-${DEB_REVISION}"
 DEB="release/cockpit-bookmarks_${DEB_VERSION}_all.deb"
 ARCHIVE="release/cockpit-bookmarks-${VERSION}.tar.gz"
