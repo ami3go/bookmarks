4dff9a79330551adbf57d790aa67aaba65a23a85
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-20 00:08:25 +0300

    Stop executing terminal compatibility checks on page load

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,6 @@ import './gotty-launcher-manager.css';
 import './update-notification.css';
 import { Application } from './app.jsx';
 import { AppProviders } from './app-providers.jsx';
-import { TerminalProviderCompatibilityNotifier } from './terminal-provider-compatibility-notifier.jsx';
 import { UpdateNotification } from './update-notification.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     createRoot(root).render(
         <AppProviders>
             <UpdateNotification />
-            <TerminalProviderCompatibilityNotifier />
             <Application />
         </AppProviders>
     );
